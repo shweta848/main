3d6131e07e1ef7ac029546a702a6729de7e58a55
shweta <manepinky1234@gmail.com>
2025-05-27 03:13:10 -0500

    first commit

---
+++ mmmm/src/mmmmm/interview.java
@@ -0,0 +1,19 @@
+package mmmmm;
+
+public class interview {
+    public static void main(String[]args) {
+	     String a[]={"SHWETAAMANE","SHWETAAMANE","SHWETAAMANE"};
+	     		
+	     int t=0;
+	     
+	     for(int i=0;i<=a.length-1;i++) {
+	   	  System.out.println(a[i]);
+	    	 
+	    	 if(a[i]=="SHWETAAMANE") {
+	    		 t++;
+	    	 }
+	    	 //System.out.println(t);
+	     }
+	     System.out.println(t);
+    }
+}
